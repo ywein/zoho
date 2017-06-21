843e2fb51b06812d5219850205897ec4e1fba279
Eric Shelley <eric@webdesignbakery.com>
2017-06-21 17:26:24 -0600

    Fixing CRM.prototype.searchRecords
    The searchRecords method was failing to encode the parentheses in the criteria param. This is a new security update made by Zoho, https://www.zoho.com/crm/help/api/security-enhancement.html.

--- test/crm.js
+++ test/crm.js
@@ -18,7 +18,7 @@ describe('Zoho CRM', function () {
     assert(zohoCRM instanceof Zoho.CRM);
   });
 
-  it('zohoCRM should have this properties', function () {
+  it('zohoCRM should have these properties', function () {
     assert(zohoCRM.protocol);
     assert(zohoCRM.host);
     assert(zohoCRM.port);
@@ -28,15 +28,16 @@ describe('Zoho CRM', function () {
     assert.equal(zohoCRM.authtoken, config.crm.authtoken);
   });
 
-  it('zohoCRM should have this public functions', function () {
+  it('zohoCRM should have these public functions', function () {
     assert.equal(typeof zohoCRM.getRecords, 'function');
     assert.equal(typeof zohoCRM.getRecordById, 'function');
     assert.equal(typeof zohoCRM.createRecord, 'function');
     assert.equal(typeof zohoCRM.updateRecord, 'function');
     assert.equal(typeof zohoCRM.deleteRecord, 'function');
+    assert.equal(typeof zohoCRM.searchRecords, 'function');
   });
 
-  it('zohoCRM should have this private functions', function () {
+  it('zohoCRM should have these private functions', function () {
     assert.equal(typeof zohoCRM._request, 'function');
     assert.equal(typeof zohoCRM._build, 'function');
   });
@@ -66,9 +67,13 @@ describe('Zoho CRM', function () {
     });
   });
 
-  // To assign created id;
-  var created_id;
+  // Used to save the created zoho lead's id;
+  let createdId;
   let sandbox;
+
+  // Used to save the updated zoho lead;
+  let updatedLead;
+
   describe('Zoho Requests', function () {
 
     before(function () {
@@ -90,6 +95,7 @@ describe('Zoho CRM', function () {
         assert.equal(typeof error, 'object');
         assert.equal(error.code, 4600);
         assert(/Unable to process your request/.test(error.message));
+
         done();
       });
     });
@@ -100,6 +106,7 @@ describe('Zoho CRM', function () {
       this.timeout(5000);
       zohoCRM._request('GET', 'fakeroute', {}, function (error, response) {
         assert.equal(httpRequestSpy.args[0][0].path.indexOf('wfTrigger=true') > -1, true);
+
         done();
       });
     });
@@ -110,6 +117,8 @@ describe('Zoho CRM', function () {
       this.params = {
         'First Name': faker.name.firstName(),
         'Last Name': faker.name.lastName(),
+        Email: faker.internet.email(),
+        Phone: faker.phone.phoneNumber(),
         Company: faker.company.companyName()
       };
       this.callback = sinon.spy();
@@ -136,7 +145,8 @@ describe('Zoho CRM', function () {
         assert.equal(error, null);
         assert.equal(typeof response, 'object');
         assert.equal(response.code, 0);
-        created_id = response.data.FL[0].content;
+        createdId = response.data.FL[0].content;
+
         done();
       });
     });
@@ -144,9 +154,10 @@ describe('Zoho CRM', function () {
 
     it('should add a note to', function (done) {
       this.timeout(3000);
-      zohoCRM.createNote(created_id, 'My first note', 'El contenido' , function (error, response) {
+      zohoCRM.createNote(createdId, 'My first note', 'El contenido' , function (error, response) {
         assert.equal(error, null);
         assert.equal(typeof response, 'object');
+
         done();
       });
     });
@@ -171,7 +182,6 @@ describe('Zoho CRM', function () {
         });
 
         done();
-
       });
     });
   });
@@ -191,7 +201,6 @@ describe('Zoho CRM', function () {
 
         done();
       });
-
     });
 
     it('should get leads without params argument', function (done) {
@@ -201,6 +210,7 @@ describe('Zoho CRM', function () {
         assert.equal(typeof response, 'object');
         assert.equal(response.code, 0);
         assert(response.data.Leads.row);
+
         done();
       });
     });
@@ -225,7 +235,7 @@ describe('Zoho CRM', function () {
     it('should get a lead by id', function (done) {
       this.timeout(3000);
       zohoCRM.getRecordById(recordType[0], {
-        id: created_id
+        id: createdId
       }, function (error, response) {
         assert.equal(error, null);
         assert.equal(typeof response, 'object');
@@ -262,20 +272,22 @@ describe('Zoho CRM', function () {
 
     it('should fail when trying to update an account without params', function () {
       this.timeout(3000);
-      zohoCRM.updateRecord(recordType[0], created_id, undefined, this.callback);
+      zohoCRM.updateRecord(recordType[0], createdId, undefined, this.callback);
       assert(this.callback.calledOnce);
       assert.notEqual(this.callback.args[0][0], null);
-      zohoCRM.updateRecord(recordType[0], created_id, {}, this.callback);
+      zohoCRM.updateRecord(recordType[0], createdId, {}, this.callback);
       assert(this.callback.calledTwice);
       assert.notEqual(this.callback.args[1][0], null);
-      zohoCRM.updateRecord(recordType[0], created_id, null, this.callback);
+      zohoCRM.updateRecord(recordType[0], createdId, null, this.callback);
       assert(this.callback.calledThrice);
       assert.notEqual(this.callback.args[2][0], null);
     });
 
     it('should update a lead', function (done) {
       this.timeout(3000);
-      zohoCRM.updateRecord(recordType[0], created_id, this.params, function (error, response) {
+
+      updatedLead = this.params; // needed for searchRecords specs
+      zohoCRM.updateRecord(recordType[0], createdId, this.params, function (error, response) {
         assert.equal(error, null);
         assert.equal(typeof response, 'object');
         assert.equal(response.code, 0);
@@ -283,6 +295,36 @@ describe('Zoho CRM', function () {
         done();
       });
 
+    });
+  });
+
+  describe('Search Zoho CRM records', function () {
+    beforeEach(function () {
+      this.callback = sinon.spy();
+    });
+
+    it('should sanitize the criteria param', function (done) {
+      var searchCriteria = { criteria: "(Last Name:Amelia{Burrows)" };
+      var sanitizedCriteria = "criteria=%28Last%20Name%3AAmelia%7BBurrows%29";
+      assert.equal(zohoCRM.sanitizeParams(searchCriteria), sanitizedCriteria);
+
+      done();
+    });
+
+    // Commenting out this test because Zoho appears to be using a
+    // search engine (e.g., Solr, Elastic, etc.) on the back end
+    // that takes 5-10 minutes for our record to appear in search
+    xit('should find contact by sanatized criteria param', function (done) {
+      this.timeout(3000);
+
+      var params = { criteria: `((First Name:${updatedLead['First Name']})AND(Last Name:${updatedLead['Last Name']}))` };
+      zohoCRM.searchRecords(recordType[0], params, function (error, response) {
+        assert.equal(error, null);
+        assert.equal(typeof response, 'object');
+        assert.equal(response.code, 0);
+
+        done();
+      });
     });
   });
 
@@ -306,7 +348,7 @@ describe('Zoho CRM', function () {
 
     it('should delete a lead', function (done) {
       this.timeout(3000);
-      zohoCRM.deleteRecord(recordType[0], created_id, function (error, response) {
+      zohoCRM.deleteRecord(recordType[0], createdId, function (error, response) {
         assert.equal(error, null);
         assert.equal(typeof response, 'object');
         assert.equal(response.code, 5000);
